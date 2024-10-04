817105e5f034b6933fc60bb77441337d2ceb0080
Angel <a_jimenez212@outlook.com>
2024-10-04 08:43:01 -0400

    Fixed header and added intro text and colors

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,10 @@ import './Main.css';
 
 export function Main() {
     return(
-        <h1>Main</h1>
+        <>
+            <h1 class = 'intro'>Little Lemon</h1>
+            <h2 class="intro">Chicago</h2>
+            <p class="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla diam vel lectus commodo rutrum. Sed orci neque, efficitur a commodo sollicitudin, ornare in lectus. </p>
+        </>
     );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -3,17 +3,19 @@ import {ReactComponent as Logo} from './Logo.svg'
 
 export function Nav() {
     return(
-        <nav class='navbar'>
-            <Logo style={{marginTop: '1rem'}}alt="Little Lemon" />
-            <ul class="navbar">
-                <li><a href="/">HOME</a></li>
-                <li><a href="/about">ABOUT</a></li>
-                <li><a href="/menu">MENU</a></li>
-                <li><a href="/reservations">RESERVATIONS</a></li>
-                <li><a href="/orderonline">ORDER ONLINE</a></li>
-                <li><a href="/login">LOGIN</a></li>
-            </ul>
-        </nav>
+        <header>
+            <nav class='navbar'>
+                <Logo style={{marginTop: '2rem', marginBottom: '2rem'}}alt="Little Lemon" />
+                <ul class="navbar">
+                    <li><a href="/">HOME</a></li>
+                    <li><a href="/about">ABOUT</a></li>
+                    <li><a href="/menu">MENU</a></li>
+                    <li><a href="/reservations">RESERVATIONS</a></li>
+                    <li><a href="/orderonline">ORDER ONLINE</a></li>
+                    <li><a href="/login">LOGIN</a></li>
+                </ul>
+            </nav>
+        </header>
     );
     
 }
